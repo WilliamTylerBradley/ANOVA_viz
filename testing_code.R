f62d90cb13285c2968673b034f7b11d41938170a
williamtylerbradley <william.tyler.bradley@gmail.com>
2016-05-14 15:54:54 -0400

    Finished 80% of graphing
    should be mostly done

--- testing_code.R
+++ testing_code.R
@@ -5,20 +5,20 @@ library(plyr)
 
 mu_1<--5
 mu_2<-0
-mu_3<-5
+mu_3<-2.5
 
 sd<-2.5
 
 n<-90
 n<-n/3
 
-data<-rbind(data.frame(trt=rep(1,n),values=rnorm(n=n,mean=mu_1,sd=sd)),
-            data.frame(trt=rep(2,n),values=rnorm(n=n,mean=mu_2,sd=sd)),
-            data.frame(trt=rep(3,n),values=rnorm(n=n,mean=mu_3,sd=sd)))
+data<-rbind(data.frame(trt=rep('Trt 1',n),values=rnorm(n=n,mean=mu_1,sd=sd)),
+            data.frame(trt=rep('Trt 2',n),values=rnorm(n=n,mean=mu_2,sd=sd)),
+            data.frame(trt=rep('Trt 3',n),values=rnorm(n=n,mean=mu_3,sd=sd)))
 data
 
 # http://stackoverflow.com/questions/1376967/using-stat-function-and-facet-wrap-together-in-ggplot2-in-r
-grid <- with(data, seq(min(values), max(values), length = 100))
+grid <- with(data, seq(min(values)-sd(values), max(values)+sd(values), length = 100))
 normaldens <- ddply(data, "trt", function(df) {
   data.frame( 
     values = grid,
@@ -26,10 +26,31 @@ normaldens <- ddply(data, "trt", function(df) {
   )
 })
 
+## Plots
+## Use color=NA to make things disappear
+## This way you don't have to worry about fixing the scales and everything
+# Plain data
 ggplot(data=data, aes(values)) +
   geom_density() +
+  geom_line(aes(y = density), data = normaldens, colour = NA) +
+  stat_function(fun=dnorm, color=NA, args=list(mean=mean(data$values), sd=sd(data$values)))  +
+  facet_wrap(~trt, ncol=1)
+
+# Data with Null
+ggplot(data=data, aes(values)) +
+  geom_density() +
+  geom_line(aes(y = density), data = normaldens, colour = NA) +
+  stat_function(fun=dnorm, color="blue", args=list(mean=mean(data$values), sd=sd(data$values)))  +
+  facet_wrap(~trt, ncol=1)
+
+# Comparison
+ggplot(data=data, aes(values)) +
+  geom_density(color=NA) +
   geom_line(aes(y = density), data = normaldens, colour = "red") +
   stat_function(fun=dnorm, color="blue", args=list(mean=mean(data$values), sd=sd(data$values)))  +
   facet_wrap(~trt, ncol=1)
+
+# Graphs are good
+# Now, summary stats
 
 
